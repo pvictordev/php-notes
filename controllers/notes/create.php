86284dbc3fa776dd9e15607a3e1116d8529b6c70
Victor Purice <victor.purice@ulbsibiu.ro>
2024-02-27 13:05:04 +0200

    divided all request for each controller

--- controllers/notes/create.php
+++ controllers/notes/create.php
@@ -1,29 +1,6 @@
 <?php
 
-use Core\Database;
-use Core\Validator;
-
-$config = require base_path("config.php");
-$db = new Database($config["database"]);
-
-$errors = [];
-if ($_SERVER["REQUEST_METHOD"] === 'POST') {
-
-    $validator = new Validator();
-
-    if (!Validator::validate($_POST['body'], 1, 1000)) {
-        $errors['body'] = 'A body of no more than 1,000 chars is required';
-    }
-
-    if (empty($errors)) {
-        $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
-            'body' => $_POST['body'],
-            'user_id' => 1
-        ]);
-    }
-}
-
 view("notes/create.view.php", [
     'heading' => 'Create a Note',
-    'errors' => $errors,
+    'errors' => [],
 ]);
--- public/index.php
+++ public/index.php
@@ -1,23 +1,19 @@
 <?php
 
-use Core\Router;
+const BASE_PATH = __DIR__ . '/../';
 
-const BASE_PATH = __DIR__ . "/../";
-
-require BASE_PATH . "Core/functions.php";
+require BASE_PATH . 'Core/functions.php';
 
 spl_autoload_register(function ($class) {
-
     $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
 
     require base_path("{$class}.php");
 });
 
-$router = new Core\Router();
+$router = new \Core\Router();
+$routes = require base_path('routes.php');
 
-$routes = require base_path("routes.php");
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-$method = isset($_POST['_method']) ? $_POST['_method'] : $_SERVER['REQUEST_METHOD'];
+$method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
 
 $router->route($uri, $method);

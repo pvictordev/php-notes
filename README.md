bd986f1f38ff078c59950c033e13c5b94eed5af5
Victor Purice <victor.purice@ulbsibiu.ro>
2024-03-05 17:36:26 +0200

    modified view

---
+++ README.md
@@ -0,0 +1,2 @@
+# PHP Notes
+## PHP Notes is a CRUD app built with PHP and MySQL
--- views/contact.view.php
+++ views/contact.view.php
@@ -32,7 +32,7 @@
             </div>
           </div>
 
-          <div class="sm:col-span-3">
+          <!-- <div class="sm:col-span-3">
             <label for="country" class="block text-sm font-medium leading-6 text-gray-900">Country</label>
             <div class="mt-2">
               <select id="country" name="country" autocomplete="country-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
@@ -70,17 +70,17 @@
               <input type="text" name="postal-code" id="postal-code" autocomplete="postal-code" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
             </div>
           </div>
+        </div> -->
+        </div>
+
+        <div class="mt-6 flex items-center justify-end gap-x-6">
+          <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+          <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
         </div>
       </div>
 
-      <div class="mt-6 flex items-center justify-end gap-x-6">
-        <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-        <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
-      </div>
-  </div>
 
-
-  </form>
+    </form>
 
   </div>
 </main>

237fecb79e8a96e25cfbcea382a98b20a1218182
Victor Purice <victor.purice@ulbsibiu.ro>
2024-03-05 15:14:20 +0200

    updated view

--- views/about.view.php
+++ views/about.view.php
@@ -4,7 +4,7 @@
 
 <?php require('partials/banner.php'); ?>
 <main>
-  <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+  <div class="mx-auto">
 
     <div class="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
       <img src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center">

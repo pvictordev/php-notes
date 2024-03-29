4721b7c73b45db69e405f67b4be873a8f4166ebc
Victor Purice <victor.purice@ulbsibiu.ro>
2024-03-29 16:31:04 +0200

    initialized registration for a new user

---
+++ controllers/registration/create.php
@@ -0,0 +1,6 @@
+<?php
+
+view("registration/create.view.php", [
+    'heading' => 'Create a Note',
+    'errors' => [],
+]);
---
+++ controllers/registration/store.php
@@ -0,0 +1,4 @@
+<?php
+
+dd($_POST);
+// should have access to the email and password that user provides

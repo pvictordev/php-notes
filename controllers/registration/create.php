b7478e176bcf1aed1cc37f4a0c1b87cbb3266ae1
Victor Purice <victor.purice@ulbsibiu.ro>
2024-04-01 17:11:05 +0300

    form validation for the new user

--- controllers/registration/create.php
+++ controllers/registration/create.php
@@ -1,6 +1,6 @@
 <?php
 
 view("registration/create.view.php", [
-    'heading' => 'Create a Note',
+    // 'heading' => 'Create a Note',
     'errors' => [],
 ]);
--- controllers/registration/store.php
+++ controllers/registration/store.php
@@ -1,4 +1,26 @@
 <?php
 
-dd($_POST);
-// should have access to the email and password that user provides
+use Core\Validator;
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+// * validate form inputs
+$errors = [];
+if (!Validator::email($email)) {
+    $errors['email'] = "Provide a valid email address.";
+}
+
+if (!Validator::validate($password, 7, 255)) {
+    $errors['email'] = "Provide a password of at least 7 chars.";
+}
+
+if (!empty($errors)) {
+    return view("registration/create.view.php", [
+        'errors' => [],
+    ]);
+}
+
+// * check if acc already exists
+    // ! if yes, redirect to the login page
+    // ! if no, save to the database and login the user and redirect
